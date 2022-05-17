992592f099106cc8082f24c79c9273ceaf4d53b3
Hank Breckenridge <hankbreckenridge@Hanks-MBP.lan>
2022-05-17 14:28:14 -0500

    Outline execute msgs

---
+++ src/execute.rs
@@ -0,0 +1,44 @@
+use cosmwasm_std::{DepsMut, Env, MessageInfo};
+
+
+pub fn execute_update_admins(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    add: Option<Vec<String>>,
+    remove: Option<Vec<String>>,
+) {
+    // Check if sender is an admin
+    
+    // Validate every address, fail if any invalid
+
+    // Perform update via map
+}
+
+pub fn execute_update_operators(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    add: Option<Vec<String>>,
+    remove: Option<Vec<String>>,
+) {
+    // Check if sender is an admin
+    
+    // Validate every address, fail if any invalid
+
+    // Perform update via map
+}
+
+pub fn execute_transfer(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    add: Option<Vec<String>>,
+    remove: Option<Vec<String>>,
+) {
+    // Check if sender is an admin
+    
+    // Validate every address, fail if any invalid
+
+    // Perform update via map
+}
